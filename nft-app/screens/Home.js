9b75d8a60cc19102e05808d40fae52a4f50eafc9
ANS <36783010+answebdev@users.noreply.github.com>
2022-04-02 11:46:39 -0700

    add components folder

--- nft-app/screens/Home.js
+++ nft-app/screens/Home.js
@@ -1,9 +1,15 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+import { COLORS, NFTData } from '../constants';
+import { NFTCard, HomeHeader, FocusStatusBar } from '../components';
 
 const Home = () => {
   return (
+    <SafeAreaView style={{ flex: 1 }}>
       <Text>Home</Text>
-  )
+    </SafeAreaView>
+  );
 };
 
 export default Home;
