30019827ec05f10e7e00f48e5bfdae6dc8234f38
ANS <36783010+answebdev@users.noreply.github.com>
2022-04-01 23:13:48 -0700

    install dependencies

--- nft-app/App.js
+++ nft-app/App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+// Video: https://www.youtube.com/watch?v=_ivIUCSOZ78&t=5s
+// Repo: https://github.com/adrianhajdin/react-native-nft-marketplace
+// Stopped at 18:00
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style='auto' /> */}
     </View>
   );
 }
